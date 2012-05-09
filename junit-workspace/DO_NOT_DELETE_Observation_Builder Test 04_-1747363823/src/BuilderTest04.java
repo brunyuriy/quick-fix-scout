081a3aefd2454a6454bebf19a20353f7167bfadc
Kivanc Muslu <kivancmuslu@gmail.com>
2012-05-09 15:55:27 -0700

    Updated the test workspace with the projects needed for ResourceUtility tests.

---
+++ junit-workspace/DO_NOT_DELETE_Observation_Builder Test 04_-1747363823/src/BuilderTest04.java
@@ -0,0 +1,10 @@
+
+public class BuilderTest04 
+{
+	private String name_;
+	
+	public void method()
+	{
+		string name2_;
+	}
+}
---
+++ junit-workspace/Resource Test 01/src/p3/ResourceTest01.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class ResourceTest01 
+{
+	// This is a comment.
+	private String name_;
+	
+	// This is a method.
+	public void m3thod()
+	{
+		// Very important computation goes here.
+	}
+}
